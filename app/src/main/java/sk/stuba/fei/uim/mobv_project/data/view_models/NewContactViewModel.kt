20fafa0f90537558f6675350c30a745cfcb0dd79
macak.nicolas@gmail.com <xmacakn@stuba.sk>
2021-11-23 20:51:47 +0100

    refactoring

--- app/src/main/java/sk/stuba/fei/uim/mobv_project/data/view_models/NewContactViewModel.kt
+++ app/src/main/java/sk/stuba/fei/uim/mobv_project/data/view_models/NewContactViewModel.kt
@@ -5,17 +5,14 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class NewContactViewModel : ViewModel(){
-    val isStringEmpty = MutableLiveData<Boolean>()
-
     var contactId = MutableLiveData<String>()
     var name = MutableLiveData<String>()
     var sourceAccount = MutableLiveData<String>()
 
-    init {
-        isStringEmpty.value = false
-        name.value = "Hextech Vidlicka"
-        sourceAccount.value = "84654"
-    }
+//    init {
+//        name.value = "Hextech Vidlicka"
+//        sourceAccount.value = "84654"
+//    }
 
     fun addToContacts(){
         sourceAccount.value?.let { Log.i("LALIHOOO", it) }
