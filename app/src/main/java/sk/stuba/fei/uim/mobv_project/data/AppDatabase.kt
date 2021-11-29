2cabd6a76354b996bec3db378ed889c5d72e0d0a
macak.nicolas@gmail.com <xmacakn@stuba.sk>
2021-11-29 22:15:51 +0100

    .allowMainThreadQueries()

--- app/src/main/java/sk/stuba/fei/uim/mobv_project/data/AppDatabase.kt
+++ app/src/main/java/sk/stuba/fei/uim/mobv_project/data/AppDatabase.kt
@@ -34,6 +34,7 @@ abstract class AppDatabase : RoomDatabase() {
                         "app_db"
                     )
                         .fallbackToDestructiveMigration()
+                        .allowMainThreadQueries()
                         .build()
                     INSTANCE = instance
                 }
